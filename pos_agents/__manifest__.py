bba68e2e509a8e6a72d9e9f73f8c6da9ae18adb6
mcanand <mcanand1998@gmail.com>
2023-11-05 15:10:54 +0530

    error fix agent
    installation error fix

--- pos_agents/__manifest__.py
+++ pos_agents/__manifest__.py
@@ -6,15 +6,20 @@
     'category': '',
     'author': 'ANAND MC',
     'summary': 'point of sale agents',
+
     'depends': ['base', 'point_of_sale', 'account'],
+    'demo': [
+
+    ],
     'data': [
-        'security/ir.model.access.csv',
         'views/agent_journal.xml',
         'views/product.xml',
+        'security/ir.model.access.csv',
         'views/pos_partner_agent.xml',
         'views/res_partner.xml',
         'views/pos_orders.xml',
     ],
+
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
--- pos_agents/models/res_partner.py
+++ pos_agents/models/res_partner.py
@@ -4,14 +4,15 @@ from odoo import fields, models, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-
-    def _get_journal(self):
-        return self.env.ref('pos_agents.gent_invoice_journal').id
-    def _get_product(self):
-        return self.env.ref('pos_agents.agent_commission').id
+    # def _get_journal(self):
+    #     journal = self.env.ref('pos_agents.agent_invoice_journal')
+    #     return journal.id if journal else ''
+    #
+    # def _get_product(self):
+    #     product =  self.env.ref('pos_agents.agent_commission')
+    #     return product.id if product else ''
 
     agent_rank = fields.Integer(default=0)
-    journal_id = fields.Many2one('account.journal', domain=[('type', '=', 'purchase')], default=_get_journal)
-    product_id = fields.Many2one('product.product', default=_get_product, readonly=True)
+    journal_id = fields.Many2one('account.journal', domain=[('type', '=', 'purchase')])
+    product_id = fields.Many2one('product.product', domain=[('detailed_type', '=', 'service')])
     agent_comm = fields.Float(string="Agent Commission (%)")
-
